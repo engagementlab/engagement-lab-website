242188379266a95e8ca630b42f8e112399ffc0f1
esalling23 <esalling23@gmail.com>
2017-03-10 13:49:34 -0500

    updated person model to add project description for those in CMAP category added cmap person partial for cmap student projects section added cmap students projects section

--- models/Person.js
+++ models/Person.js
@@ -40,12 +40,15 @@ Person.add({
 		dependsOn: { category: ['leadership', 'team', 'faculty fellows', 'lab assistants'] }, initial: true },
 	cohort: { type: String, label: 'Year(s)',
 		dependsOn: { category: ['CMAP', 'advisory board'] }, initial: true },
-
+	project: { type: Types.Markdown, label: 'Project Description', 
+		dependsOn: { category: ['CMAP', 'advisory board'] } },
+	
 	bio: { type: Types.Markdown, label: 'Bio', required: true, initial: true },
 	image: { type: Types.CloudinaryImage, label: 'Image', folder: 'site/team', note: 'Must be in square format. Will display as 192px by 192px.' },
+	
 	cmapPerson: { type: Types.Boolean, label: 'Show on CMAP page' },
-  
   category: { type: Types.Select, options: 'faculty leadership, advisory board, staff, faculty fellows, CMAP, lab assistants', default: 'team', required: true, initial: true },
+	
 	twitterURL: { type: Types.Url, label: 'Twitter' },	
 	fbURL: { type: Types.Url, label: 'Facebook' },	
 	linkedInURL: { type: Types.Url, label: 'LinkedIn' },	
@@ -89,6 +92,6 @@ Person.schema.post('save', function(next) {
 /**
  * Model Registration
  */
-Person.defaultSort = 'sortOrder';
+// Person.defaultSort = 'sortOrder';
 Person.defaultColumns = 'name, category';
 Person.register();
--- routes/views/cmap.js
+++ routes/views/cmap.js
@@ -104,36 +104,36 @@ exports = module.exports = function(req, res) {
 
     });
 
-    view.on('init', function(next) {
+    // view.on('init', function(next) {
 
-        // Get projects flagged for CMAP
-        Project.model.find({
-            'enabled': true,
-            'cmapProject': true
-        })
-        .sort([
-            ['sortOrder', 'ascending']
-        ])
-        .populate('subdirectory')
-        .exec(function(err, resultProject) {
-            _.map(resultProject, function(proj) {
+    //     // Get projects flagged for CMAP
+    //     Project.model.find({
+    //         'enabled': true,
+    //         'cmapProject': true
+    //     })
+    //     .sort([
+    //         ['sortOrder', 'ascending']
+    //     ])
+    //     .populate('subdirectory')
+    //     .exec(function(err, resultProject) {
+    //         _.map(resultProject, function(proj) {
 
-                // Create link
-                proj.href = '/projects' +
-                '/' + proj.subdirectory.key + 
-                '/' + proj.key;
-                proj.description = proj.description;
+    //             // Create link
+    //             proj.href = '/projects' +
+    //             '/' + proj.subdirectory.key + 
+    //             '/' + proj.key;
+    //             proj.description = proj.description;
 
-                return proj;
+    //             return proj;
 
-            });
+    //         });
 
-            locals.projects = resultProject;
-            next(err);
+    //         locals.projects = resultProject;
+    //         next(err);
 
-        });
+    //     });
     
-    });
+    // });
 
     // Render the view
     view.render('cmap');
