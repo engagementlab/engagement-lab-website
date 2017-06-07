9b4311f0cda2f90aaa0f1a4a4212d5febfccee5a
esalling23 <esalling23@gmail.com>
2017-06-07 14:37:54 -0400

    homepage updates

--- routes/views/index.js
+++ routes/views/index.js
@@ -51,7 +51,61 @@ exports = module.exports = function(req, res) {
 
             NewsBox.model.find({}).exec(function(err, result) {
                 locals.featured_content = result;
-                next(err);
+                
+                // locals.news = newsData.news;
+                // _.each(locals.news, function(post){
+                //     post.content = post.content.replace(/(^(By)\s+(.*)?[0-9]\s)/, '');
+                // });
+
+                // locals.featured = newsData.news.shift();
+                // locals.events = newsData.events;
+
+                // Show the 3 most recently added articles
+                Resource.model.find({ type: 'article' }, {}, {
+                    sort: { date: -1 }
+                }).limit(3).exec(function(err, articleResult){
+                    if (err) throw err;
+                    locals.articles = articleResult;
+                });
+
+                locals.twitter = {};
+
+                twitter.get('statuses/user_timeline.json?count=3', function(err, tweets, response) {
+                    
+                    if (err) throw error;
+
+                    locals.twitter.user = tweets[0].user.screen_name;
+                    locals.twitter.tweets = [];
+
+                    for (var i = 0; i < tweets.length; i++) {
+                        
+                        var tweet = tweets[i];
+                        var html = tweet.text;
+                        var entities = _.union(
+                            tweet.entities.hashtags, 
+                            tweet.entities.user_mentions, 
+                            tweet.entities.urls);
+
+                        entities = _.sortBy(entities, function(n) { return n.indices[0]; }).reverse();
+
+                        for (var j = 0; j < entities.length; j++) {
+                            var e = entities[j];
+                            html = html.splice(e.indices[1], 0, "</a>");
+                            if (e.text) {
+                                // hashtag
+                                html = html.splice(e.indices[0], 0, "<a href='https://twitter.com/hashtag/" + e.text + "/'>");
+                            } else if (e.url) {
+                                // link
+                                html = html.splice(e.indices[0], 0, "<a href='" + e.url + "'>")
+                            } else if (e.screen_name) {
+                                // mention
+                                html = html.splice(e.indices[0], 0, "<a href='https://twitter.com/" + e.screen_name + "/'>")
+                            }
+                        };
+                        locals.twitter.tweets.push({ text: html });
+                    }
+                    next(err);
+                });
             });
         });
 
