994ae64c165416d0df36de336ac2370e699bb9dd
Johnny Richardson <johnny.c.rich@gmail.com>
2017-08-16 12:32:50 -0400

    CSS rename.

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -11,7 +11,7 @@ module.exports = function() {
   // Files to minify
   config.target.files[fileOut] = [
 
-    __dirname + '/../public/styles/site.css', // site SCSS
+    __dirname + '/../public/styles/core.css', // site SCSS
   
   ];
   /*files: [{
